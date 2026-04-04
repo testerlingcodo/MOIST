f450bd4563938bd4cef69cb3ed105e77c752e287
jamesnayrbacao143-a11y <jamesnayrbacao143@gmail.com>
2026-04-04 20:44:01 +0800

    feat: connect web apps to Railway backend

--- mobile/lib/core/constants/app_constants.dart
+++ mobile/lib/core/constants/app_constants.dart
@@ -3,10 +3,7 @@ class AppConstants {
   // Android emulator: use 10.0.2.2
   // Physical Android: use your PC's current IPv4 address
   // iOS simulator: use localhost
-  static const String _host = '192.168.1.37';
-  static const int _port = 5000;
-
-  static const String baseUrl = 'http://$_host:$_port/api/v1';
+  static const String baseUrl = 'https://moist-production-7b79.up.railway.app/api/v1';
 
   static const String accessTokenKey = 'access_token';
   static const String refreshTokenKey = 'refresh_token';
